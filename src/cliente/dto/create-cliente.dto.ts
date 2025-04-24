e6b4d4e51e2880172ccddd2ca112b01fcb6f673d
Gholiveira6 <gustavoholiveira2005@gmail.com>
2025-04-23 22:37:06 -0300

    Ajuste

--- src/cliente/dto/create-cliente.dto.ts
+++ src/cliente/dto/create-cliente.dto.ts
@@ -3,9 +3,14 @@ import { isString, IsString } from 'class-validator';
 export class CreateClienteDto {
   @IsString()
   id: string;
+  @IsString()
   nome: string;
+  @IsString()
   endereco: string;
+  @IsString()
   cnpj: string;
+  @IsString()
   telefone: string;
+  @IsString()
   email: string;
 }
